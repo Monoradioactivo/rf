c9868bd5607d55b5279e65114e9c968402819d4f
Adrian Moreno <adrian.moreno.94@outlook.com>
2022-04-26 09:38:06 -0500

    Initial commit

---
+++ src/main/java/mx/simio/robinfood/RobinfoodApplication.java
@@ -0,0 +1,13 @@
+package mx.simio.robinfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/simio/robinfood/RobinfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.simio.robinfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
